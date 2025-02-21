5c855dcddbac9befd9d9c53e5895402e4dfad738
Will Bryant <will.bryant@gmail.com>
2025-02-21 22:45:41 +1300

    Remove unused abstract_unit require from test_helper
    This triggers some other loads such as capybara which doesn't work, since it's not part of the Gemfile.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,3 @@ require 'rails/test_help'
 ap_path = Gem::Specification.find_all_by_name('actionpack').first.full_gem_path
 ap_test = File.join ap_path, 'test'
 $LOAD_PATH.unshift(ap_test)
-require 'abstract_unit'
